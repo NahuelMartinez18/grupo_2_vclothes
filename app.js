377fc668ec562fb41cfe8846ad30fdc934fa0171
NahuelMartinez18 <rod.marboza@gmail.com>
2023-07-09 19:42:49 -0300

    app.js modificado con la funcionalidad para abrir un servidor, express instalado, carpeta de wireframes creada con los archivos de referencia, index.html creado.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, () => console.log("Servidor corriendo en el puerto:" + PORT));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => 
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+);
