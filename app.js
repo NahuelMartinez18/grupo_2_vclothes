7838eda6ce8d549d08f1f4aa985cdbdc50440a3a
Juan Carlos Gutierrez <cgutierrez70870@gmail.com>
2023-07-30 20:59:59 -0300

    Añadido Login

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.get("/carrito", (req, res) =>
 app.get("/register", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "register.html"))
 );
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
