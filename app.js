7c8e66429cd2d7d9aa8b5d4d8a406b943cd59538
NahuelMartinez18 <rod.marboza@gmail.com>
2023-07-30 18:50:36 -0300

    Carrito añadido

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.listen(PORT, () => console.log("Servidor corriendo en el puerto:" + PORT));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => 
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "index.html"))
+);
+
+app.get("/carrito", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "carrito.html"))
 );
